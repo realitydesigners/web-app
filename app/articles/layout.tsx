1dfd3e16c9c8526f6c2e2bbb52e7ceb70d5774f9
Raymond <raymond.luke.spartz@gmail.com>
2023-03-31 22:35:41 -0700

    added Stars component and added to /articles/layout.tsx

---
+++ app/articles/layout.tsx
@@ -0,0 +1,65 @@
+import 'tailwindcss/tailwind.css'
+
+import dynamic from 'next/dynamic'
+
+const Stars = dynamic<{}>(
+  () => import('../../components/global/Stars').then((mod) => mod.default),
+  { ssr: false }
+)
+
+import {
+  DM_Mono,
+  Do_Hyeon,
+  IBM_Plex_Mono,
+  Inter,
+  PT_Serif,
+} from '@next/font/google'
+
+const serif = PT_Serif({
+  variable: '--font-serif',
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  weight: ['400', '700'],
+})
+const sans = Inter({
+  variable: '--font-sans',
+  subsets: ['latin'],
+  // @todo: understand why extrabold (800) isn't being respected when explicitly specified in this weight array
+  // weight: ['500', '700', '800'],
+})
+const mono = IBM_Plex_Mono({
+  variable: '--font-mono',
+  subsets: ['latin'],
+  weight: ['500', '700'],
+})
+
+const dm = DM_Mono({
+  variable: '--font-dm',
+  subsets: ['latin'],
+  weight: ['300', '400', '500'],
+})
+
+const hyeon = Do_Hyeon({
+  variable: '--font-hyeon',
+  subsets: ['latin'],
+  weight: ['400'],
+})
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html
+      lang="en"
+      className={`${mono.variable} ${sans.variable} ${serif.variable}  ${hyeon.variable}`}
+    >
+      <body>
+        {' '}
+        <Stars />
+        {children}
+      </body>
+    </html>
+  )
+}
--- components/global/Stars.tsx
+++ components/global/Stars.tsx
@@ -1,7 +1,13 @@
+'use client'
+
 import Spline from '@splinetool/react-spline'
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/BEnW2F52bIxiBeFL/scene.splinecode" />
+    <div className="">
+      <div className="fixed z-0 h-screen w-screen bg-black">
+        <Spline scene="https://prod.spline.design/BEnW2F52bIxiBeFL/scene.splinecode" />
+      </div>
+    </div>
   )
 }

8becdaf8d0aaf5febb83a1eea152b319a173ee1e
Raymond <raymond.luke.spartz@gmail.com>
2023-04-04 10:36:09 -0700

    added responsive navbar in preparation for 3D scene

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Navbar from 'components/global/Navbar'
 import { HomePagePreview } from 'components/pages/home/HomePagePreview'
 import { PreviewSuspense } from 'components/preview/PreviewSuspense'
 import { PreviewWrapper } from 'components/preview/PreviewWrapper'
@@ -27,6 +28,7 @@ export default function IndexRoute() {
         <meta name="description" content="My website description"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar />
       <main className="bg-black">
         <div className="fixed z-0 h-screen w-screen">
           {' '}
--- components/global/Navbar.tsx
+++ components/global/Navbar.tsx
@@ -1,34 +1,99 @@
-import { resolveHref } from 'lib/sanity.links'
-import Link from 'next/link'
-import { MenuItem } from 'schemas/index'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import logo from 'public/favicon/logo.svg'
 
-interface NavbarProps {
-  menuItems?: MenuItem[]
-}
+const Navbar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-export function Navbar({ menuItems }: NavbarProps) {
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'unset'
+    }
+  }, [isMenuOpen])
+
   return (
-    <div className="sticky top-0 z-10 flex flex-wrap items-center gap-x-5 bg-white/80 py-4 px-4 backdrop-blur md:py-5 md:px-16 lg:px-32">
-      {menuItems &&
-        menuItems.map((menuItem, key) => {
-          const href = resolveHref(menuItem?._type, menuItem?.slug)
-          if (!href) {
-            return null
-          }
-          return (
-            <Link
-              key={key}
-              className={`text-lg hover:text-black md:text-xl ${
-                menuItem?._type === 'home'
-                  ? 'font-extrabold text-black'
-                  : 'text-gray-600'
-              }`}
-              href={href}
-            >
-              {menuItem.title}
-            </Link>
-          )
-        })}
-    </div>
+    <header className="fixed z-50 flex h-20 w-screen items-center bg-black py-4">
+      <div className="container mx-auto flex items-center justify-between px-4">
+        {/* Logo */}
+        <div className="flex items-center">
+          <Image
+            className="z-20"
+            src={logo}
+            alt="Logo"
+            width={50}
+            height={50}
+          />
+        </div>
+
+        {/* Desktop menu */}
+        <nav className="hidden space-x-8 md:flex">
+          <a
+            className="font-mono font-semibold uppercase text-slate-200"
+            href="/"
+          >
+            Home
+          </a>
+          <a className="font-mono font-semibold uppercase text-slate-200">
+            About
+          </a>
+          <a className="font-mono font-semibold uppercase text-slate-200">
+            Contact
+          </a>
+        </nav>
+
+        {/* Mobile menu button */}
+        <button
+          onClick={toggleMenu}
+          className="rounded bg-white py-2 px-4 font-semibold text-black md:hidden"
+        >
+          {isMenuOpen ? 'Close Menu' : '>'}
+        </button>
+
+        {/* Full screen menu */}
+        {isMenuOpen && (
+          <div className="fixed inset-0 z-10 bg-black  md:hidden">
+            <nav className="flex h-full flex-col items-center justify-center space-y-4">
+              <a
+                className=" font-mono text-6xl font-semibold uppercase text-slate-200"
+                href="/"
+                onClick={toggleMenu}
+              >
+                Home
+              </a>
+              <a
+                className="font-mono text-6xl font-semibold uppercase text-slate-200"
+                href="/about"
+                onClick={toggleMenu}
+              >
+                About
+              </a>
+              <a
+                className="font-mono text-6xl font-semibold uppercase text-slate-200"
+                href="/contact"
+                onClick={toggleMenu}
+              >
+                Contact
+              </a>
+
+              {/* Close button */}
+              <button
+                onClick={toggleMenu}
+                className="rounded bg-white py-4 px-4 text-4xl font-semibold text-black"
+              >
+                X
+              </button>
+            </nav>
+          </div>
+        )}
+      </div>
+    </header>
   )
 }
+
+export default Navbar

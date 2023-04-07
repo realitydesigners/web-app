e03f81cb57e900e469e16afde4796ae41a6dd881
Raymond <raymond.luke.spartz@gmail.com>
2023-04-07 12:30:46 -0700

    added first r3f to a page!

---
+++ app/r3f/page.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { PerspectiveCamera } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import Navbar from 'components/global/Navbar'
+import { HomePagePreview } from 'components/pages/home/HomePagePreview'
+import { PreviewSuspense } from 'components/preview/PreviewSuspense'
+import { PreviewWrapper } from 'components/preview/PreviewWrapper'
+import { getHomePage } from 'lib/sanity.client'
+import { getPreviewToken } from 'lib/sanity.server.preview'
+import dynamic from 'next/dynamic'
+import Head from 'next/head'
+import { notFound } from 'next/navigation'
+
+import Three from '../../components/global/Three'
+
+const Stars = dynamic<{}>(
+  () => import('../../components/global/Stars').then((mod) => mod.default),
+  { ssr: false }
+)
+
+const Logo = dynamic<{}>(
+  () => import('../../components/global/Logo').then((mod) => mod.default),
+  { ssr: false }
+)
+
+export default function IndexRoute() {
+  return (
+    <>
+      <Head>
+        <title>REALITY DESIGNERS | Building Worlds Inside Minds</title>
+        <meta name="description" content="My website description"></meta>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <main className="bg-black">
+        <div className="fixed z-0 h-screen w-screen">
+          {' '}
+          <Stars />
+        </div>
+        <div className="relative flex h-screen w-screen">
+          <div className="absolute z-0 h-full w-full">
+            <Canvas>
+              <Three />
+            </Canvas>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
---
+++ components/global/Three.tsx
@@ -0,0 +1,166 @@
+import { OrbitControls } from '@react-three/drei'
+import { PerspectiveCamera } from '@react-three/drei'
+import useSpline from '@splinetool/r3f-spline'
+
+export default function Scene({ ...props }) {
+  const { nodes, materials } = useSpline(
+    'https://prod.spline.design/VGHoKknJbcK55ull/scene.splinecode'
+  )
+  return (
+    <>
+      <color attach="background" args={['#000000']} />
+      <group {...props} dispose={null}>
+        <OrbitControls />
+        <PerspectiveCamera
+          name="Camera"
+          makeDefault={true}
+          far={100000}
+          near={70}
+          fov={45}
+          position={[0.0, 0.0, 500.0]}
+          rotation={[0, 0, 0]}
+        />
+        <group name="Hologram">
+          <group name="Animation">
+            <group
+              name="Logo"
+              position={[0.38, 115.81, -0.25]}
+              rotation={[0, 0, 0]}
+              scale={0.07}
+            >
+              <group
+                name="Left-Side"
+                position={[-97.21, -48.96, -6.63]}
+                rotation={[0, 0, Math.PI / 4]}
+                scale={1}
+              >
+                <mesh
+                  name="R"
+                  geometry={nodes.R.geometry}
+                  material={nodes.R.material}
+                  castShadow
+                  receiveShadow
+                  position={[103.34, -5, 15.4]}
+                  rotation={[Math.PI / 2, 0, 0]}
+                  scale={0.97}
+                />
+              </group>
+              <group
+                name="Right-Side"
+                position={[95.64, 49.56, -1.5]}
+                rotation={[0, 0, Math.PI / 4]}
+                scale={1}
+              >
+                <mesh
+                  name="D"
+                  geometry={nodes.D.geometry}
+                  material={nodes.D.material}
+                  castShadow
+                  receiveShadow
+                  position={[-99.94, 6.97, 10.26]}
+                  rotation={[-Math.PI / 2, 0, -Math.PI]}
+                  scale={0.97}
+                />
+              </group>
+            </group>
+            <group
+              name="TItle 2"
+              position={[-7.31, 18.66, -2.59]}
+              rotation={[-Math.PI, 0, -Math.PI]}
+            >
+              <group name="Group 5" position={[23.62, -3.18, 0]}>
+                <mesh
+                  name="Text 6"
+                  geometry={nodes['Text 6'].geometry}
+                  material={materials['Text 6 Material']}
+                  castShadow
+                  receiveShadow
+                  position={[-25.38, -46.11, 3.63]}
+                />
+                <mesh
+                  name="Text 61"
+                  geometry={nodes['Text 61'].geometry}
+                  material={materials['Text 61 Material']}
+                  castShadow
+                  receiveShadow
+                  position={[-27.11, 2, 3.92]}
+                />
+                <mesh
+                  name="Text 62"
+                  geometry={nodes['Text 62'].geometry}
+                  material={materials['Text 62 Material']}
+                  castShadow
+                  receiveShadow
+                  position={[-25.14, 43.87, 3.55]}
+                />
+              </group>
+            </group>
+            <group name="TItle" position={[5.34, 18.66, -2.59]}>
+              <group name="Group 51" position={[23.62, -3.18, 0]}>
+                <mesh
+                  name="Text 63"
+                  geometry={nodes['Text 63'].geometry}
+                  material={materials['Text 63 Material']}
+                  castShadow
+                  receiveShadow
+                  position={[-25.38, -46.11, 3.63]}
+                />
+                <mesh
+                  name="Text 64"
+                  geometry={nodes['Text 64'].geometry}
+                  material={materials['Text 64 Material']}
+                  castShadow
+                  receiveShadow
+                  position={[-27.11, 2, 3.92]}
+                />
+                <mesh
+                  name="Text 65"
+                  geometry={nodes['Text 65'].geometry}
+                  material={materials['Text 65 Material']}
+                  castShadow
+                  receiveShadow
+                  position={[-25.14, 43.87, 3.55]}
+                />
+              </group>
+            </group>
+            <mesh
+              name="Shape 3"
+              geometry={nodes['Shape 3'].geometry}
+              material={materials['Shape 3 Material']}
+              castShadow
+              receiveShadow
+              position={[-133.52, 139.06, -0.08]}
+              scale={[0.95, 0.97, 0.96]}
+            />
+            <mesh
+              name="Border"
+              geometry={nodes.Border.geometry}
+              material={nodes.Border.material}
+              castShadow
+              receiveShadow
+            />
+          </group>
+        </group>
+        <directionalLight
+          name="Directional Light"
+          castShadow
+          intensity={0.7}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-near={-10000}
+          shadow-camera-far={100000}
+          shadow-camera-left={-1000}
+          shadow-camera-right={1000}
+          shadow-camera-top={1000}
+          shadow-camera-bottom={-1000}
+          position={[-27.34, -39.9, 300]}
+        />
+        <hemisphereLight
+          name="Default Ambient Light"
+          intensity={0.75}
+          color="#eaeaea"
+        />
+      </group>
+    </>
+  )
+}

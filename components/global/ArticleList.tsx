43657ca9cfe6594ea573f915d09ad7acf6df643e
Raymond <raymond.luke.spartz@gmail.com>
2023-04-08 12:45:02 -0700

    Styled ArticleList Card

--- components/global/ArticleList.tsx
+++ components/global/ArticleList.tsx
@@ -39,29 +39,38 @@ export function ArticleList({ query }: ArticleListProps) {
         return (
           <div key={article._id}>
             <Link href={`/articles/${article.slug}`}>
-              <div className="mb-1 bg-black  p-4">
-                <h2 className="font-mono text-xl uppercase text-white">
-                  {article.title}
-                </h2>
-                <p className="mb-4  w-full font-mono text-sm text-gray-600 lg:w-2/3 lg:text-xl">
-                  {article.excerpt}
-                </p>
-                <div>
-                  {article.author && article.author.picture && (
-                    <>
-                      <Link href={`/authors/${article.author.slug.current}`}>
-                        {article.author.name}
-                      </Link>
-                    </>
-                  )}
-                </div>
-
+              <div className="group mb-1 flex p-4">
                 <Image
                   src={article.image}
                   alt={article.alt}
-                  width={300}
-                  height={300}
+                  width={200}
+                  height={200}
+                  className="h-40 w-40 transform rounded-lg object-cover transition-all group-hover:scale-105 group-hover:shadow-lg"
                 />
+                <div className="flex w-full flex-col bg-black p-4">
+                  <h2 className="font-mono text-xl uppercase text-slate-200 transition-colors duration-300 group-hover:text-white group-hover:underline">
+                    {article.title}
+                  </h2>
+                  <p
+                    className="mb-4 w-full overflow-hidden overflow-ellipsis font-mono text-sm text-gray-600 lg:w-2/3 lg:text-xl"
+                    style={{
+                      display: '-webkit-box',
+                      WebkitBoxOrient: 'vertical',
+                      WebkitLineClamp: '4',
+                    }}
+                  >
+                    {article.excerpt}
+                  </p>
+                  <div>
+                    {article.author && article.author.picture && (
+                      <>
+                        <Link href={`/authors/${article.author.slug.current}`}>
+                          {article.author.name}
+                        </Link>
+                      </>
+                    )}
+                  </div>
+                </div>
               </div>
             </Link>
           </div>

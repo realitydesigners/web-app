25d564deb9b6a052b3aec20302f47ea9112376b1
Raymond <raymond.luke.spartz@gmail.com>
2023-04-08 17:36:35 -0700

    updated favicon

--- components/global/SiteMeta.tsx
+++ components/global/SiteMeta.tsx
@@ -28,11 +28,25 @@ export function SiteMeta({
     <>
       <title>{metaTitle || demo.title}</title>
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/favicon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon.png"
+      />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="shortcut icon" href="/favicon.png" />
+      <link rel="shortcut icon" href="/favicon/favicon.png" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />

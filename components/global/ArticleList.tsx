55d8732966cd6bca8145b5f89fae20367e1c08ef
Raymond <raymond.luke.spartz@gmail.com>
2023-06-08 19:08:36 -0700

    added <Link> component in ArticleList to link to slug

--- components/global/ArticleList.tsx
+++ components/global/ArticleList.tsx
@@ -1,35 +1,38 @@
 // ArticleList.tsx
 import Image from 'next/image'
+import Link from 'next/link'
 import { FC } from 'react'
 import { ArticlesPayload } from 'schemas/index'
 
 import { urlFor } from '../../lib/urlFor' // Update path if needed
 
 interface ArticleItemProps {
-  article: ArticlesPayload
+  article: ArticlesPayload & { slug?: { current?: string } }
 }
 
 const ArticleItem: FC<ArticleItemProps> = ({ article }) => {
   return (
-    <div className=" mb-8 w-full p-6">
-      {article.image && (
-        <div className="relative mb-4 aspect-square w-full ">
-          <Image
-            src={urlFor(article.image).url() || ''}
-            alt={article.title || 'Article image'}
-            width={500}
-            height={500}
-            className="absolute inset-0 h-full w-full rounded-xl object-cover"
-          />
-        </div>
-      )}
-      <h2 className="mb-4 font-hyeon text-4xl uppercase leading-none text-white">
-        {article.title}
-      </h2>
-      <p className="xtext-lg  font-hyeon leading-tight text-gray-600">
-        {article.excerpt}
-      </p>
-    </div>
+    <Link href={`/articles/${article.slug?.current}`}>
+      <div className=" mb-8 w-full p-6">
+        {article.image && (
+          <div className="relative mb-4 aspect-square w-full ">
+            <Image
+              src={urlFor(article.image).url() || ''}
+              alt={article.title || 'Article image'}
+              width={500}
+              height={500}
+              className="absolute inset-0 h-full w-full rounded-xl object-cover"
+            />
+          </div>
+        )}
+        <h2 className="mb-4 font-hyeon text-4xl uppercase leading-none text-white">
+          {article.title}
+        </h2>
+        <p className="xtext-lg  font-hyeon leading-tight text-gray-600">
+          {article.excerpt}
+        </p>
+      </div>
+    </Link>
   )
 }
 

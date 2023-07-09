f781cd37935dbd270dabc6e25876bb5223354ca0
Raymond <raymond.luke.spartz@gmail.com>
2023-07-08 17:50:56 -0700

    replaced logo

--- components/global/Logo.tsx
+++ components/global/Logo.tsx
@@ -2,6 +2,6 @@ import Spline from '@splinetool/react-spline'
 
 export default function Logo() {
   return (
-    <Spline scene="https://prod.spline.design/mUiQTVr0Hm5LCehZ/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/8e0geo8T0dGRES0b/scene.splinecode" />
   )
 }
